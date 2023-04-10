6aa84c3c5cea47c6ed4f0e863b0a0d45dcb8d220
Alabi12 <wolo22001@gmail.com>
2023-04-10 11:32:30 +0000

    fix linter errors

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -5,30 +5,30 @@ class HousesController < ApplicationController
   end
 
   def show
-    
+    @houses = House.find(params[:id])
   end
 
   def create
-    @house = House.new(house_params)
+    @houses = House.new(house_params)
     if @houses.save
-      render json: @houses, status: :created  
+      render json: @houses, status: :created
     else
       render json: { error: @houses.errors }, status: :unprocessable_entity
     end
+  end
 
-    def update
-      @houses = House.find(params[:id])
-      if @houses.update(house_params)
-        render json: @houses
-      else
-        render json: { error: @houses.errors }, status: :unprocessable_entity
-      end
+  def update
+    @houses = House.find(params[:id])
+    if @houses.update(house_params)
+      render json: @houses
+    else
+      render json: { error: @houses.errors }, status: :unprocessable_entity
     end
-  
-    private
-  
-    def house_params
-      params.require(:house).permit(:name, :image, :price, :location, :description, :bedroom_number)
-    end
+  end
+
+  private
+
+  def house_params
+    params.require(:house).permit(:name, :image, :price, :location, :description, :bedroom_number)
   end
 end
