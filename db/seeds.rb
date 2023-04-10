7675c41e39e5bc8bdd0ab8bcb0c9318a490e4030
Nabeel Akbar <studiosnabeel@gmail.com>
2023-04-10 21:26:08 +0500

    Add Routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
  user_three = User.create!(name: 'peter')
  house_three = House.create!(name: 'The Tides At Arverne By The Sea', image: 'https://photos.zillowstatic.com/fp/00ca20c0f040f67e66bfc6738275e5fa-cc_ft_1152.webp', price: '3100', location: '190 Beach 69th St, Arverne, NY 11692', description: 'A new architectural masterpiece now dazzles the shores of the Rockaways with the addition of THE TIDES at Arverne By The Sea. Spectacular beachfront, affordable & luxurious apartments, offering modern open living spaces and high-end finishes.
 
- Amenities will include a roof deck with pool and lounge, indoor and outdoor dining experiences, private parking available for a fee, bike storage and more.', bedroom_number: 3, user: user_two)
+ Amenities will include a roof deck with pool and lounge, indoor and outdoor dining experiences, private parking available for a fee, bike storage and more.', bedroom_number: 3, user: user_three)
  Reservation.create!(start_date: '2023/05/19', end_date: '2023/05/20', user: user_three, house: house_three)
