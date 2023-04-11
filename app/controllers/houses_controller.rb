686060741abf5199886b5600710cc672cb43d9b1
Alabi12 <wolo22001@gmail.com>
2023-04-11 16:49:42 +0000

    linter errors

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -19,30 +19,12 @@ class HousesController < ApplicationController
     respond_to do |format|
       if @house.save
         format.html { redirect_to @house, notice: 'Link was successfully created.' }
-        format.json { render :show, status: :created, location: @house }
-      else
-
-        format.html { redirect_to @house, notice: 'Link was       successfully created.' }
-
-         format.json { render :show, status: :created, location:  @house }
-      else
-         format.html { render :new }
-         format.json { render json: @house.errors, status: :unprocessable_entity }
-
-        format.json { render :show, status: :created, location: @house }
       else
         format.html { render :new }
         format.json { render json: @house.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
-  def update
-    @houses = House.find(params[:id])
-    if @houses.update(house_params)
-      render json: @houses
-    else
-      render json: { error: @houses.errors }, status: :unprocessable_entity
+      end
+      format.json { render :show, status: :created, location: @house }
     end
   end
 
