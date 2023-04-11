47e513bdd2248ac2c1680671eb45afd0d1085994
Peter chendi <petzyrockchendi@gmail.com>
2023-04-11 15:58:19 +0100

    Add validation for the user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_many :houses
   has_many :reservations
 
-  validates_presence_of :name
+validates :name, presence: true, uniqueness: { message: 'Must be unique' },
+                   length: { in: 3..250, message: 'Must be a string of at least 3 characters' }
 end
