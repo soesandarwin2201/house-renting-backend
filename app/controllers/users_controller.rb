fe177134b82183f8f84badd9855bbc6eb1870a12
Alabi12 <wolo22001@gmail.com>
2023-04-11 09:32:48 +0000

    update the houses controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,21 @@ class UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else 
+      render json {
+        errors: @user.errors.full_messages
+      },
+      status: :unprocessable_entity
+    end
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
