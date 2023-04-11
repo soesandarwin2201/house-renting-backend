61260a9015757de1c98a538465e9d81359213ea0
Alabi12 <wolo22001@gmail.com>
2023-04-11 16:05:06 +0000

    Fix linter errors

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -18,11 +18,11 @@ class HousesController < ApplicationController
 
     respond_to do |format|
       if @house.save
-        format.html { redirect_to @house, notice: 'Link was       successfully created.' }
-         format.json { render :show, status: :created, location:  @house }
+        format.html { redirect_to @house, notice: 'Link was successfully created.' }
+        format.json { render :show, status: :created, location: @house }
       else
-         format.html { render :new }
-         format.json { render json: @house.errors, status: :unprocessable_entity }
+        format.html { render :new }
+        format.json { render json: @house.errors, status: :unprocessable_entity }
       end
     end
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,17 +4,18 @@ class UsersController < ApplicationController
     render json: @users
   end
 
-  def create 
+  def create
     @user = User.new(user_params)
     if @user.save
       render json: @user, status: :created
-    else 
+    else
       render json {
-        errors: @user.errors.full_messages
-      },
-      status: :unprocessable_entity
+               errors :@user.errors.full_messages
+             },
+             status: :unprocessable_entity
     end
   end
+
   private
 
   def user_params
