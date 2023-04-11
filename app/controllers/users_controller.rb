ba63b5b67da680e31c57d4531505d0b7016de021
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-04-11 20:52:07 +0630

    Create the authentication for the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
+  before_action :authorize_request, except: :create
+  before_action :find_user, except: %i[create index]
+  
   def index
     @users = User.all
     render json: @users
+  end
+
+  def create
+    @user = User.find_by(name: params[:name])
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  get 'users/index', to: 'users#index'
   get 'houses/index'
   get 'reservations/index'
+  post '/login', to: 'authentication#login'
+  post '/signup', to: 'users#create'
 end
