b664d8c9417cea082a0ede75ccae6128a6f3a4e9
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-04-15 11:33:31 +0630

    add the cor gem and modify the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
     render json: @user, status: :ok
   end
 
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
      allow do
-       origins 'http://localhost:3001/' # Replace with your React app's URL
+       origins 'localhost:3001' # Replace with your React app's URL
        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
      end
    end
