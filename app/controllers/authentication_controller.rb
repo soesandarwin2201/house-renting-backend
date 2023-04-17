129104c757aa0ae467ad1df0fcd53f8c0551c526
Alabi12 <wolo22001@gmail.com>
2023-04-17 23:09:30 +0000

    update the authentication controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,18 +3,11 @@ class AuthenticationController < ApplicationController
   # post /auth/login
 
   def login
-    @user = User.find_by(login_params)
-    if @user
-      token = JsonWebToken.encode(user_id: @user.id)
-      time = Time.now + 24.hours.to_i
-      render json: { token:, exp: time.strftime('%m-%d-%Y %H:%M'),
-                     username: @user.name }, status: :ok
-    else
-      render json: { error: 'unauthorized' }, status: :unauthorized
-    end
-  end
+    user = User
+          .find_by(email: params["user"]["email"])
+          .try(:authenticate, params["user"]["password"])
 
-  def login_params
-    params.permit(:email, :password)
   end
 end
+
+
