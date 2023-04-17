dc15ead23725cc54d3485f232ec156b2452d26f4
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-04-17 23:00:46 +0200

    Update reservation testing

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,5 @@ class Reservation < ApplicationRecord
   belongs_to :house
 
   validates :start_date, :end_date, presence: true
+  validates :city, presence: true, length: { minimum: 3, message: 'City must have 3 or more characters' }
 end
