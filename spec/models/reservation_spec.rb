b3c0c7239fc66ff0c55bf878ec109ce3b10515c4
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-04-17 23:17:17 +0200

    Remove extra line of code

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -10,10 +10,7 @@ RSpec.describe Reservation do
   it 'should contain a start date' do
     user = User.create!(name: 'robert', email: '123@gmail.com', password: '123456')
     house = House.create!(name: 'The Tides At Arverne By The Sea', image: 'https://photos.zillowstatic.com/fp/00ca20c0f040f67e66bfc6738275e5fa-cc_ft_1152.webp', price: '3100', location: '190 Beach 69th St, Arverne, NY 11692', description: 'A new architectural masterpiece now dazzles the shores of the Rockaways with the addition of THE TIDES at Arverne By The Sea. Spectacular beachfront, affordable & luxurious apartments, offering modern open living spaces and high-end finishes.Amenities will include a roof deck with pool and lounge, indoor and outdoor dining experiences, private parking available for a fee, bike storage and more.', bedroom_number: 3, user_id: user.id)
-    reservation = Reservation.create(start_date: '2023/05/19',
-                                     end_date: '2023/05/20',
-                                     user_id: user.id,
-                                     house_id: house.id)
+    reservation = Reservation.create(start_date: '2023/05/19',)
     expect(reservation.start_date.strftime("%a, %d %b %Y")).to eq('Fri, 19 May 2023')
   end
 
