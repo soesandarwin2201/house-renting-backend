21062405a6c8658df2ad42f5621a0b358dc68e1f
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-04-17 23:07:58 +0200

    Remove unwanted code

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,5 +3,4 @@ class Reservation < ApplicationRecord
   belongs_to :house
 
   validates :start_date, :end_date, presence: true
-  validates :city, presence: true, length: { minimum: 3, message: 'City must have 3 or more characters' }
 end
