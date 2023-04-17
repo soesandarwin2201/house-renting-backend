189c49cccb57d6ca83f2488b1eaadb029d2bebe7
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-04-17 22:46:51 +0200

    Add user spec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe User do
                        password: 'password')
     expect(user.name).to eql('user 1')
   end
+
+  it 'should have an email' do
+    user = User.create(name: 'user 1',
+                       email: 'useremail@email.com',
+                       password: 'password')
+    expect(user.email).to eql('useremail@email.com')
+  end
+
+  it 'should have a password' do
+    user = User.create(name: 'user 1',
+                       email: 'useremail@email.com',
+                       password: 'password')
+    expect(user.password).to eql('password')
+  end
 end
