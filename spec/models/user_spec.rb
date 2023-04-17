0c0d463b9bd38516c77d2209da70aa1fa5278d16
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-04-17 20:04:10 +0200

    Add testing for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  it 'should be valid' do
+    user = User.create(name: '',
+                       email: '',
+                       password: '')
+    expect(user).to_not be_valid
+  end
+
+  it 'should create a new user' do
+    user = User.create(name: 'user 1',
+                       email: 'useremail@email.com',
+                       password: 'password')
+    expect(user.name).to eql('user 1')
+  end
+end
