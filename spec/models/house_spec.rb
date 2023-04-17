ebaf4f5a5d5c90a0517c67a32185a796ef762bd7
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-04-17 22:33:29 +0200

    Add house spec testing

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -27,4 +27,13 @@ RSpec.describe House do
           description: 'A new architectural masterpiece now dazzles the shores of the Rockaways with the addition of THE TIDES at Arverne By The Sea. Spectacular beachfront, affordable & luxurious apartments, offering modern open living spaces and high-end finishes.')
         expect(house.location).to eql('190 Beach 69th St, Arverne, NY 11692')
       end
+
+    it 'should have a description' do
+      house = House.create(name: 'The Tides At Arverne By The Sea',
+          image: 'https://photos.zillowstatic.com/fp/00ca20c0f040f67e66bfc6738275e5fa-cc_ft_1152.webp',
+          price: '3100',
+          location: '190 Beach 69th St, Arverne, NY 11692',
+          description: 'A new architectural masterpiece now dazzles the shores of the Rockaways with the addition of THE TIDES at Arverne By The Sea. Spectacular beachfront, affordable & luxurious apartments, offering modern open living spaces and high-end finishes.')
+        expect(house.description).to eql('A new architectural masterpiece now dazzles the shores of the Rockaways with the addition of THE TIDES at Arverne By The Sea. Spectacular beachfront, affordable & luxurious apartments, offering modern open living spaces and high-end finishes.')
+      end
   end
