77ddb737bf50112dd495331924fe859a90f50a89
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-04-18 22:03:32 +0630

    genreate the swagger rspec for the reservation controller

---
+++ spec/requests/reservations_spec.rb
@@ -0,0 +1,126 @@
+require 'swagger_helper'
+
+RSpec.describe 'reservations', type: :request do
+
+  path '/reservation' do
+
+    post('create reservation') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/houses/{house_id}/reservations' do
+    # You'll want to customize the parameter types...
+    parameter name: 'house_id', in: :path, type: :string, description: 'house_id'
+
+    get('list reservations') do
+      response(200, 'successful') do
+        let(:house_id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create reservation') do
+      response(200, 'successful') do
+        let(:house_id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/houses/{house_id}/reservations/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'house_id', in: :path, type: :string, description: 'house_id'
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show reservation') do
+      response(200, 'successful') do
+        let(:house_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    patch('update reservation') do
+      response(200, 'successful') do
+        let(:house_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    put('update reservation') do
+      response(200, 'successful') do
+        let(:house_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete reservation') do
+      response(200, 'successful') do
+        let(:house_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
