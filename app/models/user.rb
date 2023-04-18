0964c4f028f1eaa663c8d2ddc3366ecfa7a94ab2
anyepeter <petzyrockchendi@gmail.com>
2023-04-18 11:29:31 +0100

    add admin role in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,8 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :password, presence: true
 
-  def admin
+  def admin?
     role == 'admin'
   end
 end
 
-# , uniqueness: { message: 'Must be unique' },
-#                    length: { in: 3..250, message: 'Must be a string of at least 3 characters' }
