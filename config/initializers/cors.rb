92b21e91396b0dca84ac3a74e6814cfb06f21f2f
Alabi12 <wolo22001@gmail.com>
2023-04-18 09:32:23 +0000

    update the cors.rb file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
@@ -7,17 +6,8 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:3001' # Replace with your React app's URL
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
-  end
-=======
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
      allow do
        origins 'localhost:3001' # Replace with your React app's URL
        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
      end
    end
-   
->>>>>>> ae44f640657df42664d737ab5967687d07806a35
