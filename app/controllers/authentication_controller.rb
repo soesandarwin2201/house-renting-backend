261e9f0878afd4b353b1d6d30501312a468c8e3c
anyepeter <petzyrockchendi@gmail.com>
2023-04-18 11:31:33 +0100

    fix login logic

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,20 +1,20 @@
 class AuthenticationController < ApplicationController
-  # skip_before_action :verify_authenticity_token
   before_action :authorize_request, except: :login
   # post /auth/login
 
   def login
-    @user = User
-      .find_by(email: params['user']['email'])
-      .try(:authenticate, params['user']['password'])
     @user = User.find_by(login_params)
     if @user
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
-      render json: { token:, exp: time.strftime('%m-%d-%Y %H:%M'),
+      render json: { token: token, exp: time.strftime('%m-%d-%Y %H:%M'),
                      username: @user.name }, status: :ok
     else
       render json: { error: 'unauthorized' }, status: :unauthorized
     end
+  end
+
+  def login_params
+    params.permit(:email, :password)
   end
 end
