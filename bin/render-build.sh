fb26c9849d3bbd0914718b0ec02fe9251293c486
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-04-20 19:09:39 +0200

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 bundle install
-bundle exec rake db:drop
-bundle exec rake db:create
-bundle exec rake db:migrate
-bundle exec rake db:seed
+bundle exec rails db:drop
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails db:seed
