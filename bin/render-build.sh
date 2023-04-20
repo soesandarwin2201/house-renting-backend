d8e48ad813c71247c08305d050e5b23f1202ba3c
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-04-20 21:51:17 +0200

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,11 +3,5 @@
 set -o errexit
 
 bundle install
-# clean
-rm -rf public
-# build
-npm install --prefix client && npm run build --prefix client
-# migrate
-bundle exec rake db:migrate
-# postbuild
-cp -a client/build/. public/
+bundle exec rails db:migrate
+bundle exec rails db:seed
