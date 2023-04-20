b63fe4a4f1314af80553e5e14f8652635a6f93b5
Nabeel Akbar <studiosnabeel@gmail.com>
2023-04-20 13:22:28 +0500

    Modify Routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,5 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/addHouse', to: 'houses#new'
 
-resources :users 
-  post "reservation", to: 'reservations#add'
-  get "reservation", to:'reservations#index'
-  resources :houses do 
-  resources :reservations 
-  end
-end
+resources :users  
+  post "reservation", to: "reservations#add"
